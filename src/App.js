4b0e82f41d4921c7e3c10ae82f43ab2b73b4573e
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-10-17 21:17:49 +0100

    Add clarifai facial recognition package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import 'tachyons';
@@ -6,6 +6,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 const particlesOptions = {
     particles: {
@@ -19,21 +20,49 @@ const particlesOptions = {
     }
 };
 
-function App() {
-  return (
-    <div className="App">
-        <Particles
-            className='particles'
-            params={particlesOptions}
-        />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
+const app = new Clarifai.App({
+    apiKey: '7c7587cf05404bbc9b7d347c33d63f7b'
+});
 
-    </div>
-  );
+class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            input: ''
+        }
+    }
+
+   onInputChange = (event) => {
+        console.log(event);
+    };
+
+    onButtonSubmit = () => {
+        app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
+            .then(response => {
+                console.log(response);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+  render () {
+      return (
+      <div className="App">
+          <Particles
+              className='particles'
+              params={particlesOptions}
+          />
+          <Navigation/>
+          <Logo/>
+          <Rank/>
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+          <FaceRecognition />
+
+      </div>
+      );
+  };
 }
 
 export default App;
