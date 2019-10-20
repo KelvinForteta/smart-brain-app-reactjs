9ffef443c1c658d82c8b80d09837e0bbf6cb56ce
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-10-20 17:24:31 +0100

    Updates

---
+++ precache-manifest.f388ee15f75ef4d5de4d4523131f7a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed83b9e7d86816a4750070a8ca5494b3",
+    "url": "/smart-brain-app-reactjs/index.html"
+  },
+  {
+    "revision": "865c3eac2cdec87da529",
+    "url": "/smart-brain-app-reactjs/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b36ea7c06b286783c3b7",
+    "url": "/smart-brain-app-reactjs/static/css/main.b36f9452.chunk.css"
+  },
+  {
+    "revision": "865c3eac2cdec87da529",
+    "url": "/smart-brain-app-reactjs/static/js/2.57927969.chunk.js"
+  },
+  {
+    "revision": "b36ea7c06b286783c3b7",
+    "url": "/smart-brain-app-reactjs/static/js/main.2375d447.chunk.js"
+  },
+  {
+    "revision": "5816e4284c85f0f6ca24",
+    "url": "/smart-brain-app-reactjs/static/js/runtime-main.0d34472e.js"
+  }
+]);
